71c1f68786af0c9cdb27298b9b9b793c80c51b42
Serik <askaroff_serik@mail.ru>
2023-10-21 10:00:25 +0300

    переделал функцию окраса

--- src/app/components/Calendar.jsx
+++ src/app/components/Calendar.jsx
@@ -1,48 +1,27 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 import './Calendar.css';
-// import 'react-calendar/dist/Calendar.css';
 
 export const GetCalendar = () => {
   const [value, onChange] = useState(new Date());
-  const [toggle, setToggle] = useState(1);
-
-  useEffect(() => {
-    const item = document.querySelectorAll('abbr');
-
-    item.forEach((e) => {
-      const days = Math.floor(
-        Date.parse(e.getAttribute('aria-label')) / (1000 * 60 * 60 * 24)
-      );
-      switch (days % 4) {
-        case 3:
-          e.parentNode.classList.add('day');
-          break;
-        case 0:
-          e.parentNode.classList.add('night');
-          break;
-        case 1:
-          e.parentNode.classList.add('dump');
-          break;
-        case 2:
-          e.parentNode.classList.add('output');
-          break;
-      }
-    });
-  }, [toggle]);
-
-  const clickBtn = () => setToggle(toggle + 1);
 
   return (
     <>
       <Calendar
-        onClickDay={clickBtn}
-        onActiveStartDateChange={clickBtn}
         onChange={onChange}
         value={value}
-        locale="en-En"
+        locale="ru-RU"
+        tileClassName={({ date }) => {
+          const days = Math.floor(
+            (Date.parse(date) / (1000 * 60 * 60 * 24)) % 4
+          );
+
+          const painting = days === 3 ? 'day' : days === 0 ? 'night' : '';
+          return painting;
+        }}
       />
     </>
   );
