276d8ec374cfe028804b3f07a0aac0de40b7146c
Serik <askaroff_serik@mail.ru>
2023-10-22 16:32:41 +0300

    добавил функцию поиска дня

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
-import { GetCalendar } from './components/Calendar';
+import { GetCalendar } from './components/Calendar/Calendar';
+import { FunctionalBlock } from './components/FunctionalBlock/FunctionalBlock';
 import styles from './page.module.css';
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <GetCalendar />
+      <FunctionalBlock />
     </main>
   );
 }
